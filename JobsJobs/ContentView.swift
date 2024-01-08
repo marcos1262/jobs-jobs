d0bbf0443d9b49b89ced0e328945cb322c1f7860
Marcos Santos <marcospaulo1262@gmail.com>
2024-01-08 10:04:16 -0300

    Initial Commit

---
+++ JobsJobs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobsJobs
+//
+//  Created by Marcos Santos on 08/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobsJobs/JobsJobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobsJobsApp.swift
+//  JobsJobs
+//
+//  Created by Marcos Santos on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct JobsJobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
